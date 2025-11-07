153c6f2ba77bf784072f08e54c772ee1394febe5
Tyler-Burke2 <smilertyler@byui.edi>
2025-11-07 13:30:13 -0700

    added some js for the subpage

--- js/subpage.js
+++ js/subpage.js
@@ -1,3 +1,32 @@
-console.log("Hello World")
+// Select elements
+const stepsWrapper = document.querySelector('.steps-wrapper');
+const steps = document.querySelectorAll('.step');
+const prevBtn = document.querySelector('.tutorial-container button:first-of-type');
+const nextBtn = document.querySelector('.tutorial-container button:last-of-type');
 
-// We will need javascript in here that allows for us to go from step to step
+let currentStep = 0;
+
+// Function to show only the current step
+function showStep() {
+  steps.forEach((step, index) => {
+    step.style.display = index === currentStep ? 'flex' : 'none';
+  });
+}
+
+// Button events
+prevBtn.addEventListener('click', () => {
+  if (currentStep > 0) {
+    currentStep--;
+    showStep();
+  }
+});
+
+nextBtn.addEventListener('click', () => {
+  if (currentStep < steps.length - 1) {
+    currentStep++;
+    showStep();
+  }
+});
+
+// Initialize first step
+showStep();
